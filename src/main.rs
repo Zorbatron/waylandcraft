23d4a6ecbb1bbeebecc9fd84a97554c08f220fbd
Fabillotic <fabillo552@gmail.com>
2025-09-27 01:20:00 +0200

    Add compositor, shm

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,77 @@ use smithay::{
             self, EventLoop,
         },
         wayland_server::{
+            self,
             backend::{ClientData, ClientId, DisconnectReason},
+            protocol::{
+                wl_surface::WlSurface,
+                wl_buffer::WlBuffer,
+            },
             Display, DisplayHandle,
         },
     },
     wayland::{
         socket::ListeningSocketSource,
+        compositor::{CompositorState, CompositorClientState, CompositorHandler},
+        buffer::BufferHandler,
+        shm::{ShmState, ShmHandler},
     },
+    delegate_compositor, delegate_shm,
 };
 
 pub struct WLCState {
     pub display_handle: DisplayHandle,
+    pub compositor_state: CompositorState,
+    pub shm_state: ShmState,
+}
+
+impl WLCState {
+    pub fn new(disp: DisplayHandle) -> Self {
+        Self {
+            display_handle: disp.clone(),
+            compositor_state: CompositorState::new::<WLCState>(&disp),
+            shm_state: ShmState::new::<WLCState>(&disp, vec![]),
+        }
+    }
+}
+
+impl CompositorHandler for WLCState {
+    fn compositor_state(&mut self) -> &mut CompositorState {
+        &mut self.compositor_state
+    }
+
+    fn client_compositor_state<'a>(
+        &self,
+        client: &'a wayland_server::Client,
+    ) -> &'a CompositorClientState {
+        &client.get_data::<WLCClient>().unwrap().compositor_state
+    }
+
+    fn commit(&mut self, surface: &WlSurface) {
+        println!("Client commited surface: {:?}", surface);
+    }
+}
+
+impl BufferHandler for WLCState {
+    fn buffer_destroyed(&mut self, _buffer: &WlBuffer) {
+    }
+}
+
+impl ShmHandler for WLCState {
+    fn shm_state(&self) -> &ShmState {
+        &self.shm_state
+    }
 }
 
 pub struct WLCClient {
+    compositor_state: CompositorClientState,
 }
 
 impl WLCClient {
     fn new() -> Self {
-        Self {}
+        Self {
+            compositor_state: CompositorClientState::default(),
+        }
     }
 }
 
@@ -47,10 +99,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Listening on: '{}'", socket.socket_name().to_str().unwrap());
 
-    let mut state = WLCState {
-        display_handle: display.handle(),
-    };
-
+    let mut state = WLCState::new(display.handle());
     let ev_handle = event_loop.handle();
 
     ev_handle.insert_source(socket, |stream, _, state| {
@@ -73,3 +122,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         state.display_handle.flush_clients().unwrap();
     }
 }
+
+delegate_compositor!(WLCState);
+delegate_shm!(WLCState);
